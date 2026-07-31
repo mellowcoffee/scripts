446d9840e532cf3ca6154555a2ab80cf6352e796
Maxwell <kssfrigyes@gmail.com>
2026-07-31 21:00:44 +0200

    moved back to wlsunset for the screen temperature script

--- desktop/color_temperature.sh
+++ desktop/color_temperature.sh
@@ -21,9 +21,7 @@ CHOICE=$(echo -e "$LIST" | rofi -dmenu -i -p "$TEMPERATURE_ICON temp" -no-custom
 
 if [[ -n "$CHOICE" ]]; then
     VALUE=${TEMPS[$CHOICE]}
-    # pkill wlsunset
-    # wlsunset -t "$VALUE" &
-    pkill hyprsunset
-    hyprsunset -t "$VALUE" &
+    pkill wlsunset
+    wlsunset -t "$VALUE" -T "$((VALUE + 1))" &
     notify-send "$SCRIPTS_TITLE" "$TEMPERATURE_ICON  Temperature set to ${VALUE}K" -a "$TEMPERATURE_NAME"
 fi
